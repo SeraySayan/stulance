82ce09e138760f82225c38fcc3a3c9dbee3da821
Seray Sayan <seraysayan00@gmail.com>
2023-05-24 00:04:17 +0300

    feat: initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+    return (
+        <div className="max-w-[1140px] mx-auto">
+            <div className="flex gap-6">
+                <div className="bg-white w-40 h-40"></div>
+                <div className="bg-white w-40 h-40"></div>
+                <div className="bg-white w-40 h-40"></div>
+            </div>
+            <div className="grid grid-cols-3 w-fit gap-6">
+                <div className="bg-pink-400 w-40 h-40"></div>
+                <div className="bg-pink-400 w-40 h-40"></div>
+                <div className="bg-pink-400 w-40 h-40"></div>
+            </div>
+        </div>
+    );
+}
