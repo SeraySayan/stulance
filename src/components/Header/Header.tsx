f1b41dd6380bf3601ca3918f645b6ca3576b91f1
Seray Sayan <seraysayan00@gmail.com>
2023-05-26 21:13:03 +0300

    Change header component and buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,24 +2,23 @@ import Image from 'next/image';
 import odtuLogo from '/public/assets/logoMetu.png';
 import Link from 'next/link';
 
-export default function Header({ displaySearch = true }: { displaySearch?: boolean }) {
+export default function Header({
+    displaySearch = true,
+    displayLogin = true,
+}: {
+    displaySearch?: boolean;
+    displayLogin?: boolean;
+}) {
     return (
         <header className="border-b-2 border-red-500">
             <div className="px-10 py-5">
                 <div className="flex justify-between item-center">
                     <div className="flex gap-5 items-center">
-                        <div className="flex flex-col items-center gap-2">
-                            <Link href="/">
-                                <Image src={odtuLogo} width={100} height={100} alt="Picture of the logo" />
-                            </Link>
+                        <Link href="/">
+                            <Image src={odtuLogo} width={100} height={100} alt="Picture of the logo" />
+                        </Link>
 
-                            <span className="font-bold">StuLance</span>
-                        </div>
-                        <div className="flex gap-5">
-                            <span>About</span>
-                            <span>Find Work</span>
-                            <span>Find Talent</span>
-                        </div>
+                        <span className="font-bold text-2xl">StuLance</span>
                     </div>
                     <div className="flex gap-5 items-center">
                         {displaySearch ? (
@@ -27,22 +26,34 @@ export default function Header({ displaySearch = true }: { displaySearch?: boole
                                 <input
                                     type="text"
                                     placeholder="Search"
-                                    className="px-3 py-2 border-2 border-red-300 rounded-full"
+                                    className="px-3 py-2 border-2 border-red-300 rounded-full hover:shadow-md hover:border-red-600"
                                     onChange={(e) => console.log(e.target.value)}
                                 />
                             </div>
                         ) : (
                             <></>
                         )}
+
+                        {displayLogin ? (
+                            <div>
+                                <Link
+                                    href="/login"
+                                    className="inline-block w-[200px] rounded-full px-6 py-2 font-bold text-center text-lg border-2 border-transparent hover:border-red-600 hover:shadow-md"
+                                >
+                                    Log In
+                                </Link>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
+
                         <div>
-                            <Link href="/login" className="inline-block rounded-full px-3 py-2">
-                                Log In
-                            </Link>
-                        </div>
-                        <div>
-                            <a href="#" className="inline-block px-3 py-2 rounded-full bg-red-300">
+                            <Link
+                                href="/signUp"
+                                className="inline-block w-[200px] rounded-full px-8 py-2  bg-red-500 text-white text-lg font-bold text-center border-2 border-transparent hover:bg-red-600 hover:border-2 hover:shadow-md"
+                            >
                                 Sign Up
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -19,12 +19,16 @@ export default function Login() {
 
     return (
         <div>
-            <Header displaySearch={false} />
+            <Header displaySearch={false} displayLogin={false} />
 
             <div className="max-w-[1140px] mx-auto p-[100px]">
-                <div className="border-2 border-red-300 rounded-lg px-5 py-4 flex flex-col gap-8">
-                    <h1 className="text-center text-xl text-[40px]">Log in to StuLance</h1>
-                    <form action="" onSubmit={handleSubmit} className="flex flex-col gap-5 px-5 py-4">
+                <div className="border-2 border-red-300 rounded-lg px-5 py-4 flex flex-col items-center">
+                    <h1 className="text-center text-[40px] font-md mt-10">Log in to StuLance</h1>
+                    <form
+                        action=""
+                        onSubmit={handleSubmit}
+                        className="flex flex-col gap-5 px-5 py-4 w-[50%] h-[400px] justify-center"
+                    >
                         <input
                             type="text"
                             required
@@ -43,7 +47,10 @@ export default function Login() {
                             onChange={handlePasswordChange}
                             className="px-3 py-2 border-2 border-red-300 rounded-lg"
                         />
-                        <button type="submit" className="bg-red-500 rounded-lg px-3 py-2 text-white text-lg">
+                        <button
+                            type="submit"
+                            className="bg-red-500 rounded-lg px-3 py-2 border-2 border-transparent text-white text-lg hover:bg-red-600 hover:border-2"
+                        >
                             Log In
                         </button>
                     </form>
